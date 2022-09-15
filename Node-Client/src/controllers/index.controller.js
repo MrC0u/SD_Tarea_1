3a17ed65d0f8bc5bc168664632e52c29690d4c55
BuildTools <unconfigured@null.spigotmc.org>
2022-09-15 01:15:40 -0300

    Servidor Node Basico implementado

---
+++ Node-Client/src/controllers/index.controller.js
@@ -0,0 +1,12 @@
+const getall = (req, res) => {
+    res.send(" En Proceso ");
+};
+
+const search = (req, res) => {
+    res.send(" En Proceso ");
+};
+
+module.exports = {
+    getall,
+    search
+}
